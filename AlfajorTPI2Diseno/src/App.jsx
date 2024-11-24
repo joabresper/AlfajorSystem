ea52494b36149171e873240f850d448b4feb8871
Camilo Aguirre <camiloo12.c1@gmail.com>
2024-11-23 23:17:38 -0300

    Login creado

--- AlfajorTPI2Diseno/src/App.jsx
+++ AlfajorTPI2Diseno/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css'
 import LoginPage from './pages/LoginPage';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [count, setCount] = useState(0)
--- AlfajorTPI2Diseno/src/pages/LoginPage.jsx
+++ AlfajorTPI2Diseno/src/pages/LoginPage.jsx
@@ -1,11 +1,110 @@
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import '../pages/LoginPage.css'
+import { Navigate } from "react-router-dom";
 
+import Logo from "../../public/logo.png";
 
 const LoginPage = () => {
-    return (
-        <div>
-            <h1>Login</h1>
+  const [inputUsername, setInputUsername] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+
+  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (event) => {
+    if (inputUsername === "mecanico" && inputPassword === "mecanico") {
+    } else if (inputUsername === "ATT" && inputPassword === "ATT") {
+    } else if (inputUsername === "Gerente" && inputPassword === "Gerente") {
+    } else {
+        setShow(true);
+    }
+  };
+
+  //usuarios del sistema
+
+
+
+  function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  return (
+    <div
+      className="sign-in__wrapper"
+      style={{ backgroundColor: '#E3D9B4' }}
+    >
+      {/* Overlay */}
+      <div className="sign-in__backdrop"></div>
+      {/* Form */}
+      <Form className="shadow p-4 bg-white rounded" onSubmit={handleSubmit}>
+        {/* Header */}
+        <img
+          className="img-thumbnail mx-auto d-block mb-2"
+          src={Logo}
+          alt="logo"
+        />
+        <div className="h4 mb-2 text-center">Log In</div>
+        {/* ALert */}
+        {show ? (
+          <Alert
+            className="mb-2"
+            variant="danger"
+            onClose={() => setShow(false)}
+            dismissible
+          >
+            Incorrect username or password.
+          </Alert>
+        ) : (
+          <div />
+        )}
+        <Form.Group className="mb-2" controlId="username">
+          <Form.Label>Usuario</Form.Label>
+          <Form.Control
+            type="text"
+            value={inputUsername}
+            placeholder="Usuario"
+            onChange={(e) => setInputUsername(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-2" controlId="password">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            type="contraseña"
+            value={inputPassword}
+            placeholder="contraseña"
+            onChange={(e) => setInputPassword(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-2" controlId="checkbox">
+          <Form.Check type="checkbox" label="Remember me" />
+        </Form.Group>
+        {!loading ? (
+          <Button className="w-100" variant="primary" type="submit">
+            Log In
+          </Button>
+        ) : (
+          <Button className="w-100" variant="primary" type="submit" disabled>
+            Logging In...
+          </Button>
+        )}
+        <div className="d-grid justify-content-end">
+          <Button
+            className="text-muted px-0"
+            variant="link"
+          >
+            Forgot password?
+          </Button>
         </div>
-    )
-}
+      </Form>
+      {/* Footer */}
+      <div className="w-100 mb-2 position-absolute bottom-0 start-50 translate-middle-x text-white text-center">
+        TPI2 Diseño de sistemas - G3 Alfajor| &copy;2024
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
