9f2d9eb2d6341ecbb87c0ade95866c5eab686246
Camilo Aguirre <camiloo12.c1@gmail.com>
2024-11-29 21:23:27 -0300

    Filtro

--- AlfajorTPI2Diseno/src/components/Turnos/ListaTurnos.jsx
+++ AlfajorTPI2Diseno/src/components/Turnos/ListaTurnos.jsx
@@ -3,10 +3,11 @@ import Table from 'react-bootstrap/Table';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import turnos from './DatosTurnos'; // Datos de turnos
+import './listaTurnos.css';
 
 const TurnosTable = () => {
     const [selectedDate, setSelectedDate] = useState(null);
-    const [filterName, setFilterName] = useState(''); // Estado para el filtro por nombre
+    const [filterName, setFilterName] = useState(''); // Filtro de nombre
 
     // Manejar cambios en el filtro de fecha
     const handleDateChange = (date) => {
@@ -20,62 +21,64 @@ const TurnosTable = () => {
 
     // Filtrar los turnos por nombre y fecha
     const filteredTurnos = turnos.filter(turno => {
-        const matchesName = turno.nombreCliente.toLowerCase().includes(filterName.toLowerCase()); // Filtrado por nombre
+        const matchesName = turno.nombreCliente.toLowerCase().includes(filterName.toLowerCase());
         const matchesDate = selectedDate
             ? new Date(turno.fecha.año, turno.fecha.mes - 1, turno.fecha.dia).toDateString() === selectedDate.toDateString()
-            : true; // Si no hay fecha seleccionada, no se filtra por fecha
-        return matchesName && matchesDate; // Cumple ambos filtros
+            : true;
+        return matchesName && matchesDate;
     });
 
     return (
-        <div>
+        <div style={{ height: "100%" }}>
             <div className="mb-3">
                 <label htmlFor="nameFilter">Filtrar por Nombre:</label>
                 <input
-                    style={{ width: '20%' }}
                     id="nameFilter"
                     type="text"
                     className="form-control"
                     placeholder="Escriba el nombre del cliente"
                     value={filterName}
-                    onChange={handleNameChange} // Actualizar el estado del filtro de nombre
+                    onChange={handleNameChange}
+                    style={{ width: '20%' }}
                 />
             </div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nombre Cliente</th>
-                        <th>DNI</th>
-                        <th>Patente</th>
-                        <th>Modelo</th>
-                        <th style={{ width: '10%' }}>
-                            <DatePicker
-                                id="dateFilter"
-                                selected={selectedDate}
-                                onChange={handleDateChange}
-                                dateFormat="dd/MM/yyyy"
-                                className="form-control"
-                                placeholderText="Fecha"
-                            />
-                        </th>
-                        <th>Hora</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredTurnos.map((turno, index) => (
-                        <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{turno.nombreCliente}</td>
-                            <td>{turno.dni}</td>
-                            <td>{turno.patente}</td>
-                            <td>{turno.modelo}</td>
-                            <td>{`${turno.fecha.dia}/${turno.fecha.mes}/${turno.fecha.año}`}</td>
-                            <td>{turno.hora}</td>
+            <div className="table-container">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nombre Cliente</th>
+                            <th>DNI</th>
+                            <th>Patente</th>
+                            <th>Modelo</th>
+                            <th style={{ width: '10%' }}>
+                                <DatePicker
+                                    id="dateFilter"
+                                    selected={selectedDate}
+                                    onChange={handleDateChange}
+                                    dateFormat="dd/MM/yyyy"
+                                    className="form-control"
+                                    placeholderText="Fecha"
+                                />
+                            </th>
+                            <th>Hora</th>
                         </tr>
-                    ))}
-                </tbody>
-            </Table>
+                    </thead>
+                    <tbody>
+                        {filteredTurnos.map((turno, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{turno.nombreCliente}</td>
+                                <td>{turno.dni}</td>
+                                <td>{turno.patente}</td>
+                                <td>{turno.modelo}</td>
+                                <td>{`${turno.fecha.dia}/${turno.fecha.mes}/${turno.fecha.año}`}</td>
+                                <td>{turno.hora}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
         </div>
     );
 };
