a6d794ad6703162969792b3fdd41c1c2d4a7cd34
Joaquin Conrado Bregant <95888562+joabresper@users.noreply.github.com>
2024-11-30 14:11:17 -0300

    Boton Cuenta agregado al menu ATT

--- AlfajorTPI2Diseno/src/pages/Turnos/MenuATT.jsx
+++ AlfajorTPI2Diseno/src/pages/Turnos/MenuATT.jsx
@@ -6,44 +6,47 @@ import Logo from "../../../public/logo.png";
 const MenuATT = () => {
     const navigate = useNavigate();
   return (
-    <div style={{width:'100vw', height:'100vh', backgroundColor:'#E3D9B4'}}>
-        <div className="container" >
-          {/* Imagen inferior izquierda */}
-          <img className="small-image" src={Logo} alt="Placeholder" />
+    <div style={{ width: '100vw', height: '100vh', backgroundColor: '#E3D9B4' }}>
+  <div className="container">
+    {/* Imagen inferior izquierda */}
+    <img className="small-image" src={Logo} alt="Placeholder" />
 
+    {/* Fondo del menú */}
+    <div className="menu-background"></div>
 
-          {/* Fondo del menú */}
-          <div className="menu-background"></div>
+    {/* Contenedor de los botones */}
+    <div className="button-container">
+      {/* Botón: Consultar Tarifas */}
+      <button className="menu-button">
+        <span className="menu-text">Consultar<br />Tarifas</span>
+      </button>
 
-          <button 
-            className="menu-button" 
-            onClick={() => navigate('/turnos')} 
-            style={{ left: '555px' }}
-          >
-            <span className="menu-text">Visualizar Turnos</span>
-          </button>
+      {/* Botón: Visualizar Turnos */}
+      <button 
+        className="menu-button"
+        onClick={() => navigate('/turnos')}
+      >
+        <span className="menu-text">Turnos</span>
+      </button>
 
-          {/* Botón: Consultar Tarifas */}
-          <button 
-            className="menu-button"
-            style={{ left: '174px' }}
-          >
-            <span className="menu-text">Consultar<br />Tarifas</span>
-          </button>
+      {/* Botón: Responder Consultas */}
+      <button className="menu-button">
+        <span className="menu-text">Responder<br />Consultas</span>
+      </button>
 
-          {/* Botón: Responder Consultas */}
-          <button 
-            className="menu-button"
-            style={{ left: '936px' }}
-          >
-            <span className="menu-text">Responder<br />Consultas</span>
-          </button>
-
-          {/* Título */}
-          <h1 className="title">¡Hola Atencion Al cliente!</h1>
-        </div>
+      {/* Botón: Cuenta */}
+      <button 
+        className="menu-button">
+        <span className="menu-text">Cuenta</span>
+      </button>
     </div>
+
+    {/* Título */}
+    <h1 className="title">¡Hola Atención al cliente!</h1>
+  </div>
+</div>
   );
+
 };
 
 export default MenuATT;
